7d7509852527c3364a43efc2719177de38ce29b8
Ajinkya Dhainje <ajinkyardhainje13@gmail.com>
2025-02-18 23:34:03 +0530

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+document.getElementById('appointment-button').addEventListener('click', function() {
+    window.location.href = "mailto:appointments@occultscience.com?subject=Tarot Reading Appointment";
+});
